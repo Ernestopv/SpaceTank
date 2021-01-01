4e57baa78c59a565a80ffb52d49944de76b53e53
Ernesto Prado <Laura Chatao@DESKTOP-BPI8S8N>
2021-01-01 17:31:14 +0100

    first commit

---
+++ TestProject/ClientApp/src/components/Home.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import Joystick from "./joystick";
+
+export class Home extends Component {
+  static displayName = Home.name;
+
+    render() {
+
+        var imageNr = 0; // Serial number of current image
+        var finished = new Array(); // References to img objects which have finished downloading
+        var paused = false;
+
+        function createImageLayer() {
+            var img = new Image();
+            img.style.position = "absolute";
+            img.style.zIndex = -1;
+            img.onload = imageOnload;
+            img.onclick = imageOnclick;
+            img.src = "http://192.168.1.82:8090/?action=snapshot&n=" + (++imageNr);
+            var webcam = document.getElementById("webcam");
+            webcam.insertBefore(img, webcam.firstChild);
+        }
+
+        // Two layers are always present (except at the very beginning), to avoid flicker
+        function imageOnload() {
+            this.style.zIndex = imageNr; // Image finished, bring to front!
+            while (1 < finished.length) {
+                var del = finished.shift(); // Delete old image(s) from document
+                del.parentNode.removeChild(del);
+            }
+            finished.push(this);
+            if (!paused) createImageLayer();
+        }
+
+        function imageOnclick() { // Clicking on the image will pause the stream
+            paused = !paused;
+            if (!paused) createImageLayer();
+        }
+
+        return (
+            <div width="100%" height="400" >
+        <div>
+            <center><img src={"http://192.168.1.82:8090/?action=stream"} width="100%" height="320" /> </center>
+        <div className="container jumbotron mt-3">
+        <Joystick />
+        </div>
+                </div>
+            </div>
+
+
+   
+    );
+  }
+}
---
+++ TestProject/ClientApp/src/components/joystick.js
@@ -0,0 +1,64 @@
+﻿import React, { Component } from "react";
+import JoyStick from "react-joystick";
+
+JoyStick.defaultProps = {
+    options: {
+        mode: "semi",
+        catchDistance: 150,
+        color: "gray",
+        size: 180
+    },
+    containerStyle: {
+        width: "50%",
+        height: "40vh",
+        position: "relative",
+        background: "linear-gradient(to right, #E684AE, #79CBCA, #77A1D3)"
+    }
+};
+
+const joyOptions = {
+    mode: "semi",
+    catchDistance: 150,
+    color: "white",
+    size: 50
+};
+
+const containerStyle = {
+    position: "relative",
+    height: "40vh",
+    width: "100%",
+    background: "linear-gradient(to right, #77A1D3, #79CBCA, #77A1D3)"
+};
+
+class Control extends Component {
+    constructor() {
+        super();
+        this.managerListener = this.managerListener.bind(this);
+    }
+
+    managerListener(manager) {
+        manager.on("move", (e, stick) => {
+            console.log(stick.direction);
+        });
+        manager.on("end", () => {
+            console.log("stop");
+        });
+    }
+
+    render() {
+        const { classes } = this.props;
+        return (
+            <div>
+            <JoyStick
+        joyOptions={joyOptions}
+        containerStyle={containerStyle}
+        managerListener={this.managerListener}
+            />
+            </div>
+    );
+}
+}
+
+
+
+export default Control;
